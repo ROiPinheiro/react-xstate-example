c7255b65fc868c64df87aa6d4c4204e392d70fc8
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2020-05-20 22:39:17 -0300

    Updates

---
+++ precache-manifest.3ec1e40778b56fc4a97737f50d0e4d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1c3596712b027e1de8d191a91729f3",
+    "url": "/react-xstate-example/index.html"
+  },
+  {
+    "revision": "07f3605a7f0b078bae4d",
+    "url": "/react-xstate-example/static/js/2.0359ac20.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/react-xstate-example/static/js/2.0359ac20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8461e43442c89285668d",
+    "url": "/react-xstate-example/static/js/main.dae9182b.chunk.js"
+  },
+  {
+    "revision": "2a415c7bec71d001e0c3",
+    "url": "/react-xstate-example/static/js/runtime-main.f76ffd18.js"
+  }
+]);
